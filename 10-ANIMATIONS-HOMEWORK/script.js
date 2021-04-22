5aa7d06f1db9c8b4b0e9497a74c5f22f4600378e
Oksana Svystun <svystuno.j@gmail.com>
2021-04-22 22:38:29 +0300

    Added first animation

---
+++ 10-ANIMATIONS-HOMEWORK/script.js
@@ -0,0 +1,5 @@
+document.getElementById('swrds').onclick = function() {
+    document.getElementById('fs').classList.toggle('click-up');
+    document.getElementById('ss').classList.toggle('click-cross-one');
+    document.getElementById('ts').classList.toggle('click-cross-two');
+}
