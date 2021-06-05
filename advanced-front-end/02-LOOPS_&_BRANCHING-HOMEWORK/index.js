f0f9be3cb1c3ac0c27e08caddf29aca6d09394f5
Oksana Svystun <svystuno.j@gmail.com>
2021-06-05 22:11:12 +0300

    Fixed bug with empty string

--- advanced-front-end/02-LOOPS_&_BRANCHING-HOMEWORK/index.js
+++ advanced-front-end/02-LOOPS_&_BRANCHING-HOMEWORK/index.js
@@ -3,9 +3,9 @@ let secondNumber;
 let firstNumberInt;
 let secondNumberInt;
 
-// Checks or input is an integer and a number function
+// Checks or input is an integer, a number and not empty string function
 function integer(number) {
-  if ((Number.isInteger(Number(number)) && Number(number)) || Number(number)==0) {
+  if ((Number.isInteger(Number(number)) && Number(number)) || Number(number) === 0 && !Number(number) === '') {
     return true;
   } else {
     return false;
@@ -16,7 +16,7 @@ function integer(number) {
 do {
   firstNumber = +prompt("Enter first (integer) number.");
   firstNumberInt = integer(firstNumber);
-} while (!firstNumberInt === true );
+} while (!firstNumberInt === true);
 
 // Do prompt while function isn't true and firstNumber!= secondNumber
 do {
@@ -30,11 +30,11 @@ const maxNumber = Math.max(firstNumber, secondNumber);
 const minNumber = Math.min(firstNumber, secondNumber);
 let sum = 0;
 
+// Check whether to include even numbers in the sum
 if (includeEven) {
     for (let i = minNumber; i <= maxNumber; i++) {
       if (i % 2 != 0) {
         sum += i;
-        console.log(sum)
       }
     }
   } else {
