423d7b8435c8269504c6b6fd8298e9f1f037ef7c
Oksana Svystun <svystuno.j@gmail.com>
2021-06-09 00:25:59 +0300

    Optimised code

--- advanced-front-end/02-LOOPS_&_BRANCHING-HOMEWORK/index.js
+++ advanced-front-end/02-LOOPS_&_BRANCHING-HOMEWORK/index.js
@@ -5,7 +5,7 @@ let secondNumberInt;
 
 // Checks or input is an integer, a number and not empty string function
 function integer(number) {
-  if ((Number.isInteger(Number(number)) && Number(number)) || Number(number) === 0 && !Number(number) === '') {
+  if ((Number.isInteger(Number(number)) && Number(number)) || !Number(number)) {
     return true;
   } else {
     return false;
@@ -16,13 +16,13 @@ function integer(number) {
 do {
   firstNumber = +prompt("Enter first (integer) number.");
   firstNumberInt = integer(firstNumber);
-} while (!firstNumberInt === true);
+} while (!firstNumberInt);
 
 // Do prompt while function isn't true and firstNumber!= secondNumber
 do {
   secondNumber = +prompt("Enter ANOTHER second (integer) number.");
   secondNumberInt = integer(secondNumber);
-} while (!secondNumberInt === true || firstNumber == secondNumber);
+} while (!secondNumberInt || firstNumber == secondNumber);
 
 const includeEven = confirm("Skip even numbers?");
 
@@ -31,16 +31,12 @@ const minNumber = Math.min(firstNumber, secondNumber);
 let sum = 0;
 
 // Check whether to include even numbers in the sum
-if (includeEven) {
-    for (let i = minNumber; i <= maxNumber; i++) {
-      if (i % 2 != 0) {
-        sum += i;
-      }
-    }
-  } else {
-    for (let i = minNumber; i <= maxNumber; i++) {
-      sum += i;
-    }
+
+for (let i = minNumber; i <= maxNumber; i++) {
+  if (includeEven && i % 2 === 0) {
+    continue;
+  }
+  sum += i;    
 }
 
 alert(`Sum is ${sum}`);
